4216213b6b89f2bf38a86fc39ebb3cb7406b4f65
Akshaysg19 <akshaysgujjar@gmail.com>
2024-01-14 19:41:48 +0530

    Fifth Commit

--- page_objects.py
+++ page_objects.py
@@ -4,8 +4,15 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.ChromeOptions()
 driver.add_argument('--headless')
+driver = webdriver.Chrome()
 def open_page():
     # create webdriver page
     print("Open page")
-    driver = webdriver.Chrome()
+
     driver.get("https://www.geeksforgeeks.org/")
+    # get element
+    element = driver.find_element(By.XPATH,"//div[@class='ant-row ant-row-center gfg_home_page_search_heading']")
+    # close current window
+    print(element.text)
+    driver.close()
+
