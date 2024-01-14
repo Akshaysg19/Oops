e186bb219af940e445fb8ad0d691a45d9b514593
Akshaysg19 <akshaysgujjar@gmail.com>
2024-01-14 18:24:56 +0530

    Forth Commit

--- page_objects.py
+++ page_objects.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-
+driver = webdriver.ChromeOptions()
+driver.add_argument('--headless')
 def open_page():
     # create webdriver page
     print("Open page")
+    driver = webdriver.Chrome()
     driver.get("https://www.geeksforgeeks.org/")
