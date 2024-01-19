b3928a3a679353fb6fab3e77bd46b34cd07784f0
Akshaysg19 <47499998+Akshaysg19@users.noreply.github.com>
2024-01-19 16:35:18 +0530

    Update page_objects.py

--- page_objects.py
+++ page_objects.py
@@ -8,7 +8,6 @@ driver = webdriver.Chrome()
 def open_page():
     # create webdriver page
     print("Open page")
-    #opening the page
     driver.get("https://www.geeksforgeeks.org/")
     # get element
     element = driver.find_element(By.XPATH,"//div[@class='ant-row ant-row-center gfg_home_page_search_heading']")
